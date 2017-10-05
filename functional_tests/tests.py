2f2f6520638bcc5fd58b545e69065b91064fcc89
willshyr <williamshyr@williams-mbp-2.attlocal.net>
2017-10-04 23:51:58 -0700

    Added new list views

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -120,6 +120,5 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Satisfied, they both go back to sleep
 
-
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
--- lists/views.py
+++ lists/views.py
@@ -4,16 +4,13 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-    """
-    new_item_text: hold the POST contents, or the empty string
-    """
-    if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text) # create new Item without needing to call .save()
-        return redirect('/lists/the-only-list-in-the-world')
-
     return render(request, 'home.html')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    new_item_text = request.POST['item_text']
+    Item.objects.create(text=new_item_text) # create new Item without needing to call .save()
+    return redirect('/lists/the-only-list-in-the-world/')
