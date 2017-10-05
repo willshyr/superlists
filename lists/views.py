40bd20e57735725a54d1323a7d238a4ecd031aed
willshyr <williamshyr@williams-mbp-2.attlocal.net>
2017-10-05 02:51:30 -0700

    new URL + view for adding to existing lists. FT passes.

--- lists/views.py
+++ lists/views.py
@@ -8,11 +8,15 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=list_)
-    return render(request, 'list.html', {'items': items})
+    return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
     new_item_text = request.POST['item_text']
     list_ = List.objects.create()
     Item.objects.create(text=new_item_text, list=list_) # create new Item without needing to call .save()
+    return redirect(f'/lists/{list_.id}/')
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect(f'/lists/{list_.id}/')
