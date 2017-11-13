63059bf1e3c547e4bc5be7070fb6754e4371d61e
willshyr <williamshyr@Williams-MacBook-Pro-2.local>
2017-11-13 14:12:47 -0800

    Split unit tests into two files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
 from lists.models import Item, List
 
+
 class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-
 
     # def test_displays_all_list_items(self):
     #     Item.objects.create(text='itemey 1')
@@ -27,36 +27,7 @@ class HomePageTest(TestCase):
     #     self.assertEqual(Item.objects.count(), 0)
 
 
-class ItemModelTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, list_)
-
-class ListViewText(TestCase):
+class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
         list_ = List.objects.create()
@@ -88,8 +59,7 @@ class ListViewText(TestCase):
         self.assertEqual(response.context['list'], correct_list)
 
 
-class NewListText(TestCase):
-
+class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
         self.client.post('/lists/new', data={'item_text': 'A new list item'})
 
@@ -102,14 +72,14 @@ class NewListText(TestCase):
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
-class NewItemTest(TestCase):
 
+class NewItemTest(TestCase):
     def test_can_save_a_POST_request_to_an_existing_list(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
 
         self.client.post(f'/lists/{correct_list.id}/add_item', data={'item_text': 'A new item for an existing list'}
-        )
+                         )
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
