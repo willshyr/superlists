7f7c4269196898d7df78469a730bf16f14e579d4
willshyr <williamshyr@Williams-MacBook-Pro-2.local>
2017-11-14 11:34:24 -0800

    Added ExistingListItemForm class to forms.py

--- lists/forms.py
+++ lists/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 from lists.models import Item
 
-
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
 
 class ItemForm(forms.models.ModelForm):
@@ -27,3 +27,15 @@ class ItemForm(forms.models.ModelForm):
         self.instance.list = for_list
         return super().save()
 
+
+class ExistingListItemForm(ItemForm):
+    def __init__(self, for_list, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.instance.list = for_list
+
+    def validate_unique(self):
+        try:
+            elf.instance.validate_unqiue()
+        except ValidationError as e:
+            e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
+            self._update_errors(e)
