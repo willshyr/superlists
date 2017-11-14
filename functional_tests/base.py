8956748c48ccf91c7c34477c0bcea850a2e93112
willshyr <williamshyr@Williams-MacBook-Pro-2.local>
2017-11-13 21:39:48 -0800

    Rename all item input ids and names. Still borken

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -55,6 +55,8 @@ class FunctionalTest(StaticLiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
+    def get_item_input_box(self):
+        return self.browser.find_element_by_id('id_text')
 
 # if __name__ == '__main__':
 #     unittest.main(warnings='ignore')
--- lists/views.py
+++ lists/views.py
@@ -11,7 +11,7 @@ def home_page(request):
 def new_list(request):
     list_ = List.objects.create()
     # Create new Item without needing to call .save()
-    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item.objects.create(text=request.POST['text'], list=list_)
     try:
         item.full_clean()
     except ValidationError:
@@ -24,7 +24,7 @@ def new_list(request):
 
 # def add_item(request, list_id):
 #     list_ = List.objects.get(id=list_id)
-#     Item.objects.create(text=request.POST['item_text'], list=list_)
+#     Item.objects.create(text=request.POST['text'], list=list_)
 #     return redirect(f'/lists/{list_.id}/')
 
 
@@ -34,7 +34,7 @@ def view_list(request, list_id):
 
     if request.method == 'POST':
         try:
-            item = Item(text=request.POST['item_text'], list=list_)
+            item = Item(text=request.POST['text'], list=list_)
             item.full_clean()
             item.save()
             return redirect(list_)
