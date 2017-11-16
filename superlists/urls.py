d09b3ed09b2d46b7abdddaacdf6c5ffd7a944693
willshyr <williamshyr@Williams-MacBook-Pro-2.local>
2017-11-16 02:44:24 -0800

    Unittest send_mail using monkeypatching by hand

--- superlists/urls.py
+++ superlists/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from lists import views as list_views
 from lists import urls as list_urls
+from accounts import urls as accounts_urls
 # from django.contrib import admin
 from lists import views
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^$', list_views.home_page, name='home'),
     url(r'^lists/', include(list_urls)),
-    # url(r'^accounts/', include(accounts_urls)),
+    url(r'^accounts/', include(accounts_urls)),
 ]
