115d64208c0d90cc8a6c72fc1712ce43b5403c23
willshyr <williamshyr@Williams-MacBook-Pro-2.local>
2017-11-16 17:00:38 -0800

    Added unittest for login view

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import redirect
 from django.core.mail import send_mail
+from django.contrib import messages
 # Create your views here.
 
 def send_login_email(request):
     email = request.POST['email']
     send_mail(
         'Your login link for Superlists',
-        'body text tbc',
+        'Use this link to log in',
         'noreply@superlists',
         [email])
 
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in."
+    )
+
+    return redirect('/')
+
+
+def login(request):
     return redirect('/')
