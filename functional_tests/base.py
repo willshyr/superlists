77d7f8a1a54aed748f13d8ef04201eb0d953dfec
willshyr <williamshyr@Williams-MacBook-Pro-2.local>
2018-01-10 16:26:48 +0800

    Make test_my_lists run the management command on the staging server if we are on it

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -5,6 +5,7 @@ from selenium.common.exceptions import WebDriverException
 import unittest
 import time
 import os
+from .server_tools import reset_database
 MAX_WAIT = 10  # Maximum amount of time we're prepared to wait
 
 
@@ -25,9 +26,10 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
+        self.staging_server = os.environ.get('STAGING_SERVER')
+        if self.staging_server:
+            self.live_server_url = 'http://' + self.staging_server
+            reset_database(self.staging_server)
 
     def tearDown(self):
         self.browser.quit()
